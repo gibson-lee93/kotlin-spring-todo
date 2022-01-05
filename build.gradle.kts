4640ecdb9bb9079e63798bb57754ac5c4e573281
Gibson <gb.lee.gibson@gmail.com>
2022-01-05 11:57:43 +0900

    chore: model mapper setting
    - add 'model mapper' dependency
    - register 'model mapper' bean

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.modelmapper:modelmapper:2.4.4")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
