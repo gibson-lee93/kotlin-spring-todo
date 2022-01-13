51de82337ecb96fd8f0fb38873139b8808da6b65
Gibson <gb.lee.gibson@gmail.com>
2022-01-13 16:19:37 +0900

    Start over todo project: create todo feature
    - delete all files and start over
    - apply TDD style
    - start with create todo feature

--- src/main/kotlin/com/example/todo/controller/TodoController.kt
+++ src/main/kotlin/com/example/todo/controller/TodoController.kt
@@ -1,42 +1,17 @@
 package com.example.todo.controller
 
-import com.example.todo.dto.TodoDto
-import com.example.todo.entity.Todo
+import com.example.todo.domain.Todo
 import com.example.todo.service.TodoService
 import org.springframework.http.HttpStatus
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/todos")
-class TodoController(private val todoService: TodoService) {
+class TodoController(val service: TodoService) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody @Validated todoDto: TodoDto): Todo { //Dto에서 validation 사용하려면 @Validated 필수
-        return todoService.create(todoDto)
-    }
-
-    @GetMapping("/{id}")
-    fun detail(@PathVariable(name = "id") id: Long): Todo {
-        return todoService.detail(id)
-    }
-
-    @GetMapping
-    fun list(): List<Todo> {
-        return todoService.list()
-    }
-
-    @PutMapping("/{id}")
-    fun update(
-        @PathVariable(name = "id") id: Long,
-        @RequestBody @Validated todoDto: TodoDto
-    ): Todo {
-        return todoService.update(id, todoDto)
-    }
-
-    @DeleteMapping("/{id}")
-    fun delete(@PathVariable(name = "id") id: Long): String {
-        return todoService.delete(id)
+    fun create(@RequestBody todo: Todo): Todo {
+        return service.create(todo)
     }
 }
--- src/main/kotlin/com/example/todo/exception/APIException.kt
+++
@@ -1,7 +0,0 @@
-package com.example.todo.exception
-
-import org.springframework.http.HttpStatus
-
-class APIException(status: HttpStatus, message : String) : RuntimeException(message) {
-    val status = status
-}
